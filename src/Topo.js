a122cdb7033745850a0aa00cd821d790e071943a
Nivea <nivea@respondeai.com.br>
2021-08-31 14:12:07 -0300

    preparando para o review

--- src/Topo.js
+++
@@ -1,8 +0,0 @@
-export default function Topo(){
-    return (
-        <div className = "topo">
-            <h1 className = "nomeEmpresa righteous">FoodCamp</h1>
-            <h1 className = "slogan roboto">Sua comida em 6 minutos</h1>
-        </div>
-    );
-}
